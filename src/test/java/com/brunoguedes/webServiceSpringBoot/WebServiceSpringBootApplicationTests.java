8c8a1318166c0d6e990cd8f0062015bcbdbb82b5
Bruno Guedes <brunoguedesit@gmail.com>
2020-11-26 16:39:21 -0300

    Project created

---
+++ src/test/java/com/brunoguedes/webServiceSpringBoot/WebServiceSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunoguedes.webServiceSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

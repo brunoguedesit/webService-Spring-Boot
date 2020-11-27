903d66e00344b5ff5335513b424b1a733a2f7c86
Bruno Guedes <brunoguedesit@gmail.com>
2020-11-27 18:38:36 -0300

    Service layer, component registration

---
+++ src/main/java/com/brunoguedes/webServiceSpringBoot/services/UserService.java
@@ -0,0 +1,26 @@
+package com.brunoguedes.webServiceSpringBoot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.brunoguedes.webServiceSpringBoot.entities.User;
+import com.brunoguedes.webServiceSpringBoot.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}

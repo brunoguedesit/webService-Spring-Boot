5d7dc97b0e1582cfe8fd59d36868eda386de714a
Bruno Guedes <brunoguedesit@gmail.com>
2020-12-02 11:44:26 -0300

    Exception handling - findById

---
+++ src/main/java/com/brunoguedes/webServiceSpringBoot/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.brunoguedes.webServiceSpringBoot.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not foud, id " + id);
+	}
+}
